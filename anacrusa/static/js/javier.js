de4b175d8a4fce88a398231e2ff8e56498fe495e
jroca72 <jroca72@gmail.com>
2019-04-10 15:13:32 +0200

    '5'

---
+++ anacrusa/static/js/javier.js
@@ -0,0 +1,13 @@
+function poner(objeto, valor){
+	
+        var g1 = new JustGage({
+          id: objeto,
+          value: valor,
+          donut: true, 
+          pointer: true, 
+          simbol: 'º',
+          min: 0,
+          max: 45,
+        });
+
+};
